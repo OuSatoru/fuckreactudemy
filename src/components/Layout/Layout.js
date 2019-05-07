cd77423408120bc710255fddc3cc4fe7b3d7a2d3
Wang Cong <ousatoru@hotmail.com>
2019-05-07 16:12:05 +0800

    135 6

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import classes from './Layout.module.css'
+import Toobar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toobar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
